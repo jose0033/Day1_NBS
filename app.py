7cf27b88dfb1d54d3e481d7ab8919634945cf025
jose0033 <101925195+jose0033@users.noreply.github.com>
2022-03-21 10:21:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
-
 from flask import Flask
-
-
-
-
-app = Flask(__name__)
+app=Flask(__name__)
 
 from flask import request, render_template
-import joblib 
+import joblib
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        rates = request.form.get("rates")
+        rates=request.form.get("rates")
         print(rates)
-        model = joblib.load("DBS")
+        model = joblib.load('DBS_regression')
         pred = model.predict([[float(rates)]])
         print(pred)
-        s = "The predicted DBS share price is " + str(pred[0][0])
-        return(render_template("index.html", result=s))
+        s = "Predicted DBS Share Price: " + str(pred)
+        print(s)
+        return(render_template("index.html", results=s))
+
     else:
-        return(render_template("index.html", result="No Input"))
-if __name__ =="__main__":
+        return(render_template("index.html", results="DBS Share Price Prediction"))
+
+
+if __name__=="__main__":
     app.run()
-
-
-
-
-# In[ ]:
+    
